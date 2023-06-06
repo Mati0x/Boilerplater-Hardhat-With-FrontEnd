84be0d591faeb111ed097fa8176164c0006762ca
Matias <mantitaaa89@gmail.com>
2023-06-06 14:30:17 -0300

    feat: initial ui

--- frontend/pages/index.jsx
+++ frontend/pages/index.jsx
@@ -1,7 +1,15 @@
+import { ConnectButton } from "@rainbow-me/rainbowkit";
 export default function Home() {
   return (
-    <div>
-      <main className="text-3xl">Hello World</main>
-    </div>
+    <>
+      <div className="h-screen w-full bg-gradient-to-r from-sky-500 to-indigo-500 ">
+        <nav className="p-4 border-black flex justify-end fixed w-full">
+          <ConnectButton></ConnectButton>
+        </nav>
+        <main className="h-full flex items-center justify-center">
+          <h1 className="font-mono text-5xl">Cool UI Here</h1>
+        </main>
+      </div>
+    </>
   );
 }
